a2e3e1afdca4d14883ed7c8c7138a516cac24b24
g022c1011 <g022c1011@g.neec.ac.jp>
2024-07-10 11:59:40 +0900

    env file update

--- selenium_env/Scripts/activate.bat
+++ selenium_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=G:\マイドライブ\学校\授業\3年 前期\木18 システム開発グループ演習\QRAttendanceSystem\selenium_env
+set VIRTUAL_ENV=C:\Users\sasho\Documents\QRAttendanceSystem\selenium_env
 
 if not defined PROMPT set PROMPT=$P$G
 
