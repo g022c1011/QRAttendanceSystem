39ade39ae492f2731231858ef905ba7cbc833be4
g022c1011 <g022c1011@g.neec.ac.jp>
2024-07-19 06:25:00 +0900

    ログイン画面 作成

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -13,3 +13,16 @@ def signup_view(request):
         form = SignUpForm()
     param = {"form": form}
     return render(request, "accounts/signup.html", param)
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            if user:
+                login(request, user)
+                return redirect(to="/accounts/login/#undefined/")
+    else:
+        form = LoginForm()
+    param = {"form": form}
+    return render(request, "accounts/login.html", param)
