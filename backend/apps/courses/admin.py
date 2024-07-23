a8084abb35dc42890e732764bd4b20cf48a16d3f
g022c1011 <g022c1011@g.neec.ac.jp>
2024-07-23 16:14:30 +0900

    admin画面更新

--- backend/apps/courses/admin.py
+++ backend/apps/courses/admin.py
@@ -7,8 +7,10 @@ class SessionInline(admin.TabularInline):
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'start_time')
+    list_display = ('name','start_date','start_time')
     search_fields = ('name',)
+    list_filter = ('start_date',)
+    ordering = ('start_date',)
     list_filter = ('start_time',)
     ordering = ('start_time',)
     inlines = [SessionInline]
