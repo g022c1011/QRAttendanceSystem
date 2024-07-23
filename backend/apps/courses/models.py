b374f6e41b58b39970778a5c7b4c66a78dbb68a9
g022c1011 <g022c1011@g.neec.ac.jp>
2024-07-23 15:22:52 +0900

    授業開始日時の追加

--- backend/apps/courses/models.py
+++ backend/apps/courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
+    start_date = models.DateField()
     start_time = models.TimeField()
 
     def __str__(self):
