d3b57159dc219c5ecb34935671845ffa27e71037
g022c1011 <g022c1011@g.neec.ac.jp>
2024-07-25 14:21:42 +0900

    アカウント管理に教員、学生に分割

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -4,9 +4,20 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path("signup/", views.signup_view, name="signup"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
     path("home/", views.home_view, name="home"),
+
+    path("signup_account_selection/", views.signup_account_selection_view, name="signup_account_selection"),
+    # path("login_account_selection/", views.login_account_selection_view, name="login_account_selection"),
+
+    path("teacher_signup/", views.teacher_signup_view, name="teacher_signup"),
+    path("student_signup/", views.student_signup_view, name="student_signup"),
+
+    # path("teacher_login/", views.teacher_login_view, name="teacher_login"),
+    # path("student_login/", views.student_login_view, name="student_login"),
+    path("login/", views.login_view, name="login"),
+
     path("userpage/", views.userpage_view, name="userpage"),
+
+    path("logout/", views.logout_view, name="logout"),
+
 ]
--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,19 +1,47 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
-from .forms import LoginForm, SignUpForm
+from .forms import LoginForm, TeacherSignUpForm, StudentSignUpForm
 from django.contrib.auth.decorators import login_required
 
-def signup_view(request):
+# ホーム
+def home_view(request):
+    return render(request, "accounts/home.html")
+
+# サインイン
+def signup_account_selection_view(request):
     if request.method == "POST":
-        form = SignUpForm(request.POST)
+        account_type = request.POST.get('account_type')
+        if account_type in ['student']:
+            request.session['account_type'] = account_type
+            return redirect('accounts:student_signup')
+        elif account_type in ['teacher']:
+            request.session['account_type'] = account_type
+            return redirect('accounts:teacher_signup')
+    return render(request, "accounts/account_selection.html")
+
+def teacher_signup_view(request):
+    if request.method == "POST":
+        form = TeacherSignUpForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("accounts:home")
     else:
-        form = SignUpForm()
+        form = TeacherSignUpForm()
     param = {"form": form}
-    return render(request, "accounts/signup.html", param)
+    return render(request, "accounts/teacher_signup.html", param)
 
+def student_signup_view(request):
+    if request.method == "POST":
+        form = StudentSignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:home")
+    else:
+        form = StudentSignUpForm()
+    param = {"form": form}
+    return render(request, "accounts/student_signup.html", param)
+
+# ログイン
 def login_view(request):
     if request.method == "POST":
         form = LoginForm(request, data=request.POST)
@@ -27,14 +55,52 @@ def login_view(request):
     param = {"form": form}
     return render(request, "accounts/login.html", param)
 
+# def login_account_selection_view(request):
+#     if request.method == "POST":
+#         account_type = request.POST.get('account_type')
+#         if account_type in ['student']:
+#             request.session['account_type'] = account_type
+#             return redirect('accounts:student_login')
+#         elif account_type in ['teacher']:
+#             request.session['account_type'] = account_type
+#             return redirect('accounts:teacher_login')
+#     return render(request, "accounts/account_selection.html")
+
+# def teacher_login_view(request):
+#     if request.method == "POST":
+#         form = LoginForm(request, data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             if user and user.account_type == 'teacher':
+#                 login(request, user)
+#                 return redirect("accounts:userpage")
+#     else:
+#         form = LoginForm()
+#     param = {"form": form}
+#     return render(request, "accounts/teacher_login.html", param)
+
+# def student_login_view(request):
+#     if request.method == "POST":
+#         form = LoginForm(request, data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             if user:
+#                 login(request, user)
+#                 return redirect("accounts:userpage")
+#     else:
+#         form = LoginForm()
+#     param = {"form": form}
+#     return render(request, "accounts/student_login.html", param)
+
+# ログアウト
 @login_required
 def logout_view(request):
     logout(request)
     return render(request, "accounts/logout.html")
 
-def home_view(request):
-    return render(request, "accounts/home.html")
-
+# ユーザーページ
 @login_required
 def userpage_view(request):
     return render(request, "accounts/userpage.html")
+
+
