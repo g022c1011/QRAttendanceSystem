0ab8476ce4c87235d202d6e3f1280839b758536b
g022c1011 <g022c1011@g.neec.ac.jp>
2024-07-25 14:54:50 +0900

    ファイル整理

--- backend/apps/students/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Student
-
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('student_id', 'name', 'google_account')
-    search_fields = ('name', 'student_id')
-    list_filter = ('google_account',)
-    ordering = ('student_id',)
--- backend/apps/students/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Student(models.Model):
-    student_id = models.CharField(max_length=50, unique=True, primary_key=True)
-    name = models.CharField(max_length=100)
-    google_account = models.EmailField(unique=True)
-
-    def __str__(self):
-        return self.name
